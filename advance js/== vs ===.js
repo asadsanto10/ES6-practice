1a8a9a3ba58899db7d94c5160c6e147a0cf46535
asadsanto10 <santo.meridian@gmail.com>
2020-08-07 10:45:45 +0600

    new problem addd

---
+++ advance js/== vs ===.js
@@ -0,0 +1,17 @@
+// double equal(==) mane bujhai shudu valu check korbe 
+// tripple equal(===) mane bujai valu plus type duitai check korbe
+
+const a = 2;
+const b = 2;
+if(a == b){
+    console.log('true');
+}
+else{
+    console.log('false');
+}
+
+if (a === b) {
+    console.log('true');
+} else {
+    console.log('false');
+}
---
+++ advance js/null vs undefine.js
@@ -0,0 +1,34 @@
+//let a;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+
+const result = add(12, 15)
+console.log(result);
+// console.log(a);
+
+let pakhi;
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+
+const premik = { name: "smart dude", phone: 458421 };
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9]
+console.log(ages[11]);
+
+
+function doSomething(x, y) {
+    console.log(y); 
+} 
+doSomething(32);
